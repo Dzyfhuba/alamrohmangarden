ea864ed0b416260e187f7c0ea24fd9bcee0f9058
Dzyfhuba <hafidz21ub@gmail.com>
2023-02-04 23:36:19 +0700

    full migration from raw react

--- moved/android/app/src/main/java/com/hafidzubaidillah/alamrohmangarden/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.hafidzubaidillah.alamrohmangarden;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- moved/src/Variables.tsx
+++
@@ -1,4 +0,0 @@
-const host = 'http://localhost:3333'
-// const host = 'https://resources.alamrohmangarden.com'
-
-export {host}

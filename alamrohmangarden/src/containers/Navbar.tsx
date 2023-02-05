d58fc8878a7580aa0a3dc253a07f3b0fb7ba9e62
Dzyfhuba <hafidz21ub@gmail.com>
2023-02-05 09:28:26 +0700

    add i18n

--- alamrohmangarden/src/containers/Navbar.tsx
+++ alamrohmangarden/src/containers/Navbar.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Logo from '../../public/logo.png'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-  console.log('asd')
+  
   return (
     <nav className='h-14 shadow px-12 flex justify-between capitalize font-bold tracking-[0.5px]'>
       <Link href={'/'}>
-        <Image src={'/logo.png'} alt={'Alam Rohman Garden'} height={56} width={261} />
+        <Image src={Logo} alt={'Alam Rohman Garden'} className='h-full w-full' fill={false} />
       </Link>
-      <div className="flex items-center">
-        <Link href={'/services'} className='h-14 px-5 flex items-center hover:shadow-hover'>services</Link>
+      <div className="hidden sm:flex items-center">
+        <Link href={'/services'} className='h-14 px-5 flex items-center hover:shadow-hover' locale='en'>services</Link>
+        <Link href={'/services'} className='h-14 px-5 flex items-center hover:shadow-hover' locale='id'>jasa</Link>
         <Link href={'/articles'} className='h-14 px-5 flex items-center hover:shadow-hover'>articles</Link>
         <Link href={'/about'} className='h-14 px-5 flex items-center hover:shadow-hover'>about</Link>
       </div>
--- alamrohmangarden/src/pages/index.tsx
+++ alamrohmangarden/src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ locale }: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(locale);
+  
   return (
     <>
       dashboard
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = ({ locale }) => {
+  return {
+    props: {
+      locale
+    }
+  }
 }
